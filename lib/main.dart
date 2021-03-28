d17aa4139fdabc96f99d5a4a17e3363242332d1e
djamel <djamel.sbar@gmail.com>
2021-03-28 15:40:54 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -87,25 +87,7 @@ class _MyAppState extends State<MyApp> {
         home: Scaffold(
           appBar: AppBar(title: Text("Quiz App")),
           body: Container(
-            child: ListView.builder(
-                itemCount: _questions.length,
-                itemBuilder: (ctx, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Card(
-                      color: Theme.of(ctx).primaryColor,
-                      child: Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Center(
-                          child: Text( 
-                            _questions[index]['questionText'],
-                            style: TextStyle(fontSize: 25),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
+            
           ),
         ));
   }
